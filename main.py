c5a89acfb466a741e4124cff9c6dbd1ab8419922
Xiaoyang Liu <144244539+ERHUTUZI123@users.noreply.github.com>
2025-11-28 12:56:38 +0800

    Enhance data splitting and add SVC classifier training
    Added stratified split explanation and SVC classifier training with GridSearchCV.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import pickle
 
 from skimage.io import imread
 from skimage.transform import resize
@@ -6,6 +7,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
 from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
 # prepare data
 input_dir = 'D:\\xiaoyangliu\\image_classification\\clf-data'
@@ -39,14 +41,31 @@ labels = np.asarray(labels)
 # train / test split
 # test_size=20%: 20% data for test and 80% data for train
 # shuffle=True: shuffle data to avoid bias 
-# stratify=labels: 
-#   Keep the proportion of the tag among all labels and the 
-#   proportion of the element from that label among 
-#   every sets unchanged.
-#   for instance: 80% label red and 20% label blue
+# stratify=labels:
+#   Perform a stratified split. The train and test sets will preserve 
+#   the same class proportion as in the original dataset.
+#   Example: if the original labels are 80% red and 20% blue,
+#   then both the train set and test set will maintain 80% red
+#   and 20% blue.
 x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, shuffle=True, stratify=labels)
 
-
 # train classifier
+classifier = SVC()
+
+parameters = [{'gamma':[0.01, 0.001, 0.0001], 'C': [1, 10, 100, 1000]}]
+
+grid_search = GridSearchCV(classifier, parameters)
+
+
+grid_search.fit(x_train, y_train)
 
 # test performance
+best_estimator = grid_search.best_estimator_
+
+y_prediction = best_estimator.predict(x_test)
+
+score = accuracy_score(y_prediction, y_test)
+
+print('{}% of samples were correctly classified'.format(str(score * 100)))
+
+pickle.dump(best_estimator, open('./model.p', 'wb'))
